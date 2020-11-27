08a54e1badd185b5b68cd6e0575a8d464aa18ea4
mounika_dasari <64784385+mounika-dasari@users.noreply.github.com>
2020-11-27 23:25:45 +0530

    Create 7.js

---
+++ weekend assigment3/7.js
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<HTML>
+<HEAD>
+<script>
+let request=null;
+try{              
+
+                                  /
+request=new XMLHttpRequest(); 
+request.open('GET','https://restcountries.eu/rest/v2/all',true);	
+
+request.send();													
+}
+catch(err)			
+{
+	alert("An Exception has happened in fetching the API with error message: "+err.message);   
+}
+request.onload=function(){											
+	var data=JSON.parse(this.response);								
+	let countriesUsingUSD = data.filter(item => item.currencies[0].code=='USD').map(x=> x.name);   
+																			
+	console.log('Countries using USD as currency are - ' + countriesUsingUSD.join(';')); 
+};
+</script>
+</HEAD>
+</HTML>
